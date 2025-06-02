48ef4669f0e75c7a9a336b63762ecfcce04f7597
Lucas Agromayor <lucas@thebrighthotel.com>
2025-06-02 02:02:48 -0300

    ref companies with transfers use cases

--- src/modules/companies/application/usecases/getCompaniesWithTransfersByEffectiveDate.usecases.ts
+++ src/modules/companies/application/usecases/getCompaniesWithTransfersByEffectiveDate.usecases.ts
@@ -1,33 +1,48 @@
 import { Inject, Injectable } from "@nestjs/common";
 import { CompanyEntityDto } from "../../domain/company.entity";
 import { COMPANY_REPOSITORY, CompanyRepository } from "../../domain/company.repository";
-import { GetTransfersByEffectiveDateUseCases } from "src/modules/transfers/application/usecases/getTransfersByEffectiveDate.usecases";
+import { TRANSFER_REPOSITORY, TransferRepository } from "src/modules/transfers/domain/transfer.repository";
+import { deprecate } from "node:util";
 
 @Injectable()
 export class GetCompaniesWithTransfersByEffectiveDateUseCase {
     constructor(
         @Inject(COMPANY_REPOSITORY)
         private readonly companyRepository: CompanyRepository,
-        private readonly getTransfersByRegistrationDateUseCases: GetTransfersByEffectiveDateUseCases,
+        @Inject(TRANSFER_REPOSITORY)
+        private readonly transferRepository: TransferRepository,
     ) { }
 
-    async execute(from: Date, to: Date): Promise<CompanyEntityDto[]> {
-        const transfersInLastMonth = await this.getTransfersByRegistrationDateUseCases.execute(from, to);
+    /**
+     * @deprecated("Use execute instead")
+     * @param from 
+     * @param to 
+     * @returns 
+     */
+    private async executeDeprecated(from: Date, to: Date): Promise<CompanyEntityDto[]> {
+        const companiesWithTransfer = await this.transferRepository.findUniqueCompaniesByEffectiveDate(from, to);
 
-        if (!transfersInLastMonth || transfersInLastMonth.length === 0) {
+        if (!companiesWithTransfer || companiesWithTransfer.length === 0) {
             return [];
         }
 
-        const uniqueCompaniesWithTransfersInLastMonth = [...new Set(transfersInLastMonth.map((t) => t.companyIdFrom))];
-
-        const companies = (await Promise.all(
-            uniqueCompaniesWithTransfersInLastMonth.map((t) => this.companyRepository.findById(t))
-        )).filter((c): c is CompanyEntityDto => !!c);
-
+        const companies = await this.companyRepository.findByIds(companiesWithTransfer);
+        
         if (!companies || companies.length === 0) {
             return [];
         }
 
         return companies;
     }
+
+    async execute(from: Date, to: Date): Promise<CompanyEntityDto[]> {
+        const companiesWithTransfer = await this.transferRepository.findCompaniesWithTransfersInDateRange(from, to);
+
+        if (!companiesWithTransfer || companiesWithTransfer.length === 0) {
+            return [];
+        }
+
+        return companiesWithTransfer;
+    }
+
 }
